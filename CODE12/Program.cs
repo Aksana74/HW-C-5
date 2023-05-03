68b9ac14fa03a4dc87c2acab36604b59860882b9
Aksana74 <detkovao185@gmail.com>
2023-05-03 19:32:50 +0300

    CODE12

---
+++ CODE12/Program.cs
@@ -0,0 +1,30 @@
+﻿
+// Задайте массив заполненный случайными положительным трехзначными числами.
+///Напишите программу, которая покажет количество четных чисел в массиве. 
+
+
+Console.Write ("Введите количество элементов массива:");
+int x=Convert.ToInt32 (Console.ReadLine());
+int[] randomArray=new int[x];
+void mas(int x)
+{
+    for (int i=0; i<x;i++)
+{
+randomArray[i]=new Random().Next(99,999);
+Console.Write(randomArray[i]);
+}
+}
+int kol   (int []randomArray)
+{
+int kol=0;
+for ( int i=0; i<randomArray.Length;i++)
+
+{
+    if (randomArray[i]%2==0)
+    kol=kol+1;
+}
+return kol;
+}
+
+mas(x);
+Console.Write($"Количество четных чисел в массиве:{kol (randomArray)}");
